b84e0b5115bcc6f0d881ccd7cbb9818f90354198
Gavin <1707344@mukilteo.wednet.edu>
2022-02-04 08:15:57 -0800

    Add project files.

---
+++ OOPQuiz/Card.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPQuiz
+{
+    class Card
+    {
+        /// <summary>
+        /// The Number on the card
+        /// </summary>
+        int number;
+        public enum Type
+        {
+            Spade, 
+            Clover,
+            Heart,
+            Diamond
+        }
+        public enum Color{
+            Red,
+            Black
+        }
+
+        Color cardColor;
+        Type cardType;
+        float width;
+        float height;
+        string backColor;
+
+        public Card(int number, Color cardColor, Type cardType)
+        {
+            this.number = number;
+            this.cardColor = cardColor;
+            this.cardType = cardType;
+        }
+
+        public Card(int number, Color cardColor, Type cardType, float width, float height, string backColor) : this(number, cardColor, cardType)
+        {
+            this.width = width;
+            this.height = height;
+            this.backColor = backColor;
+        }
+
+        public static Card[] Shuffle(Card[] cards)
+        {
+            Random random = new Random();
+            for (int i = 0; i < cards.Length; i++)
+            {
+                int randomCardIndex = random.Next(0, cards.Length);
+                Card card = cards[i];
+                Card randomCard = cards[randomCardIndex];
+
+                cards[i] = null;
+                cards[randomCardIndex] = null;
+
+                cards[i] = randomCard;
+                cards[randomCardIndex] = card;
+            }
+
+            return cards;
+        }
+
+        public void Throw()
+        {
+            Random random = new Random();
+
+            string text = random.Next(0, 5) > 3 ? "You threw the card perfectly!" : "Some how the card went backwards";
+            Console.WriteLine(text);
+        }
+
+        public void Display()
+        {
+            string type = "";
+            switch (cardType)
+            {
+                case Type.Clover:
+                    type = "♣";
+                    break;
+                case Type.Spade:
+                    type = "♠";
+                    break;
+                case Type.Heart:
+                    type = "♥";
+                    break;
+                case Type.Diamond:
+                    type = "♦";
+                    break;
+            }
+
+            string color = "";
+            switch (cardColor)
+            {
+                case Color.Red:
+                    color = "Red";
+                    break;
+                case Color.Black:
+                    color = "Black";
+                    break;
+
+            }
+
+            Console.WriteLine($"Card type: {type}\nColor: {color}\nNumber: {number}\n\n\n");
+        }
+    }
+}
---
+++ OOPQuiz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOPQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card1 = new Card(3, Card.Color.Black, Card.Type.Diamond);
+            Card card2 = new Card(5, Card.Color.Red, Card.Type.Spade);
+            Card card3 = new Card(9, Card.Color.Black, Card.Type.Clover);
+
+            Card[] cards = { card1, card2, card3 };
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i].Display();
+            }
+
+            Console.WriteLine("Shuffle\n\n");
+            cards = Card.Shuffle(cards);
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i].Display();
+            }
+        }
+    }
+}
